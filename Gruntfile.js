70405c237a246d437665faa822377548ca1dc7ab
BA <kau@connecticum.de>
2014-11-18 21:23:38 +0100

    introducing a new variable to define the github username

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,20 +3,19 @@ module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
 
 	grunt.initConfig( {
+		github_username: '2ndkauboy',
 		wp_plugin_path: ( undefined == grunt.option( 'plugin' ) ) ? '' : '../' + grunt.option( 'plugin' ) + '/',
 		wp_readme_to_markdown: {
-			github_2ndkauboy: {
+			target: {
 				files: {
 					'<%= wp_plugin_path + "readme.md" %>' : '<%= wp_plugin_path + "readme.txt" %>'
 				},
 				options: {
-					screenshot_url: 'https://raw.githubusercontent.com/2ndkauboy/{plugin}/master/assets/{screenshot}.png'
+					screenshot_url: 'https://raw.githubusercontent.com/<%= github_username %>/{plugin}/master/assets/{screenshot}.png'
 				}
 			}
 		}
 	} );
-	
-	grunt.config( 'files', { readme_txt : readme_md } );				
 
-	//grunt.registerTask( 'default', ['wp_readme_to_markdown'] );
+	grunt.registerTask( 'readme', ['wp_readme_to_markdown:target'] );
 };
